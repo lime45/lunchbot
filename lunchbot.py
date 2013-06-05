5cfc3d19d2ef68594f6d381a5d4936c967e5a38b
Dan Dedrick <ddedrick@lexmark.com>
2013-06-05 08:42:41 -0400

    Initial lunchbot

---
+++ lunchbot.py
@@ -0,0 +1,142 @@
+#! /usr/bin/env python
+#
+# Example program using irc.bot.
+#
+# Joel Rosdahl <joel@rosdahl.net>
+
+"""A simple example bot.
+
+This is an example bot that uses the SingleServerIRCBot class from
+irc.bot.  The bot enters a channel and listens for commands in
+private messages and channel traffic.  Commands in channel messages
+are given by prefixing the text by the bot name followed by a colon.
+It also responds to DCC CHAT invitations and echos data sent in such
+sessions.
+
+The known commands are:
+
+    stats -- Prints some channel information.
+
+    disconnect -- Disconnect the bot.  The bot will try to reconnect
+                  after 60 seconds.
+
+    die -- Let the bot cease to exist.
+
+    dcc -- Let the bot invite you to a DCC CHAT connection.
+"""
+
+import irc.bot
+import irc.strings
+from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
+from random import randint
+
+class TestBot(irc.bot.SingleServerIRCBot):
+    index=0;
+    places = [];
+    def __init__(self, channel, nickname, server, port=6667):
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
+        self.channel = channel
+
+    def on_nicknameinuse(self, c, e):
+        c.nick(c.get_nickname() + "_")
+
+    def on_welcome(self, c, e):
+        c.join(self.channel)
+	c.notice(self.channel,"Hey guy. I'm " + c.get_nickname() + ". I'm here to help you pick somewhere to go for lunch");
+        c.notice(self.channel,"To make a suggestion just send me a public or private message with the word lunch followed by your suggestion");
+        c.notice(self.channel,"I will decide when you tell me to decide and when you tell me clear I will clear my suggestions");
+
+
+    def on_privmsg(self, c, e):
+        self.do_command(e, e.arguments[0])
+
+    def on_pubmsg(self, c, e):
+        a = e.arguments[0].split(":", 1)
+        if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
+            self.do_command(e, a[1].strip())
+        return
+
+    def on_dccmsg(self, c, e):
+        c.privmsg("You said: " + e.arguments[0])
+
+    def on_dccchat(self, c, e):
+        if len(e.arguments) != 2:
+            return
+        args = e.arguments[1].split()
+        if len(args) == 4:
+            try:
+                address = ip_numstr_to_quad(args[2])
+                port = int(args[3])
+            except ValueError:
+                return
+            self.dcc_connect(address, port)
+
+    def do_command(self, e, args):
+        nick = e.target
+	if nick == self.connection.get_nickname() :
+	    nick = e.source.nick;
+        c = self.connection
+	cmd = args.split(" ",1);
+
+        if cmd[0] == "disconnect":
+            self.disconnect()
+        elif cmd[0] == "die":
+            self.die()
+        elif cmd[0] == "stats":
+            for chname, chobj in self.channels.items():
+                c.notice(nick, "--- Channel statistics ---")
+                c.notice(nick, "Channel: " + chname)
+                users = chobj.users()
+                users.sort()
+                c.notice(nick, "Users: " + ", ".join(users))
+                opers = chobj.opers()
+                opers.sort()
+                c.notice(nick, "Opers: " + ", ".join(opers))
+                voiced = chobj.voiced()
+                voiced.sort()
+                c.notice(nick, "Voiced: " + ", ".join(voiced))
+	elif cmd[0] == "lunch":
+            self.places.insert(self.index, cmd[1]);
+            self.index = self.index + 1;
+            c.notice(nick, e.source.nick + " suggests " + cmd[1]);
+        elif cmd[0] == "decide":
+            selection = randint(0,self.index-1);
+            c.notice(nick, "Why don't you go to " + self.places[selection] + "?" );
+        elif cmd[0] == "options":
+            place_str = ",or ".join(map(str,self.places));
+            c.notice(nick, "You could go to " + place_str + ".");
+        elif cmd[0] == "clear":
+            del self.places[0:self.index];
+            c.notice(nick, "Ok I'm forgetting all of your suggestions");
+        elif cmd[0] == "dcc":
+            dcc = self.dcc_listen()
+            c.ctcp("DCC", nick, "CHAT chat %s %d" % (
+                ip_quad_to_numstr(dcc.localaddress),
+                dcc.localport))
+        else:
+            c.notice(nick, "Not understood: " + cmd[0])
+
+def main():
+    import sys
+    if len(sys.argv) != 4:
+        print "Usage: testbot <server[:port]> <channel> <nickname>"
+        sys.exit(1)
+
+    s = sys.argv[1].split(":", 1)
+    server = s[0]
+    if len(s) == 2:
+        try:
+            port = int(s[1])
+        except ValueError:
+            print "Error: Erroneous port."
+            sys.exit(1)
+    else:
+        port = 6667
+    channel = sys.argv[2]
+    nickname = sys.argv[3]
+
+    bot = TestBot(channel, nickname, server, port)
+    bot.start()
+
+if __name__ == "__main__":
+    main()

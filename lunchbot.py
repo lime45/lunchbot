6c0b376462e5c413e14fd5b9f9928540e396d48e
Dan Dedrick <ddedrick@lexmark.com>
2013-07-15 09:05:59 -0400

    A little bit of clean up.

--- lunchbot.py
+++ lunchbot.py
@@ -1,29 +1,4 @@
 #! /usr/bin/env python
-#
-# Example program using irc.bot.
-#
-# Joel Rosdahl <joel@rosdahl.net>
-
-"""A simple example bot.
-
-This is an example bot that uses the SingleServerIRCBot class from
-irc.bot.  The bot enters a channel and listens for commands in
-private messages and channel traffic.  Commands in channel messages
-are given by prefixing the text by the bot name followed by a colon.
-It also responds to DCC CHAT invitations and echos data sent in such
-sessions.
-
-The known commands are:
-
-    stats -- Prints some channel information.
-
-    disconnect -- Disconnect the bot.  The bot will try to reconnect
-                  after 60 seconds.
-
-    die -- Let the bot cease to exist.
-
-    dcc -- Let the bot invite you to a DCC CHAT connection.
-"""
 
 import irc.bot
 import irc.strings
@@ -145,13 +120,11 @@ class TestBot(irc.bot.SingleServerIRCBot):
         if nick == self.connection.get_nickname() :
            nick = e.source.nick;
         if self.last_nick == e.source.nick:
-           print("nick match");
            self.last_nick_count+=1;
            if self.last_nick_count > 4:
               c.privmsg(nick,"Hey " + e.source.nick + ", why don't you give someone else a chance to talk");
               self.last_nick_count = 0;
         else:
-           print("no match " + self.last_nick + " " + e.source.nick);
            self.last_nick = e.source.nick;
            self.last_nick_count = 0;
         a = e.arguments[0].split(":", 1)
